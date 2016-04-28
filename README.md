d444d6f81425c2441d7f65197e1cc7819a67a062
deju <juju@ChristopherdeiMac.local>
2016-04-28 14:03:52 +0800

    update casperjs login demo

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 ### run 
     
     casper test login.js
-
+    // for proxy
+    casper test login.js --proxy=127.0.0.1:8888
--- login.js
+++ login.js
@@ -1,8 +1,8 @@
+var x = require("casper").selectXPath;
 
 
 casper.test.begin('login success test', 1, function(test) {
     casper.start('http://softtest.topnology.com.cn:12824/ecp/Index.jsp', function() {
-        //test.assertExists('#navbar-example');
         this.sendKeys('.FormTable #_JUI_1_ input', 'administrator');
         this.sendKeys('.FormTable #_JUI_2_ input', '111111');
         this.click('.LoginButton');
@@ -10,11 +10,6 @@ casper.test.begin('login success test', 1, function(test) {
         	console.log(this);
         	test.pass('login test success');
         })
-        /*
-        this.waitUntilVisible('.LoginButton', function() {
-        	console.log(this);
-            test.pass('login success');
-        }); */
     }).run(function() {
         test.done();
     });
@@ -23,7 +18,6 @@ casper.test.begin('login success test', 1, function(test) {
 
 casper.test.begin('login fail test', 1, function(test) {
     casper.start('http://softtest.topnology.com.cn:12824/ecp/Index.jsp', function() {
-        //test.assertExists('#navbar-example');
         this.sendKeys('.FormTable #_JUI_1_ input', 'administrator');
         this.sendKeys('.FormTable #_JUI_2_ input', '01');
         this.click('.LoginButton');
@@ -31,11 +25,48 @@ casper.test.begin('login fail test', 1, function(test) {
         	console.log(this);
         	test.pass('login fail test success');
         })
-        /*
-        this.waitUntilVisible('.LoginButton', function() {
-        	console.log(this);
-            test.pass('login success');
-        }); */
+    }).run(function() {
+        test.done();
+    });
+});
+
+
+
+
+// this.test.assertExists({
+//     type: 'xpath',
+//     path: '//*[@id="MenuZone"]/div[2]/div[1]/div/div[4]'
+// }, 'the element exists');
+
+casper.test.begin('add employe test', 1, function(test) {
+    casper.start('http://softtest.topnology.com.cn:12824/ecp/Index.jsp', function() {
+        //test.assertExists('#navbar-example');
+        this.sendKeys('.FormTable #_JUI_1_ input', 'administrator');
+        this.sendKeys('.FormTable #_JUI_2_ input', '111111');
+        this.click('.LoginButton');
+        this.waitForSelector('.QsDarkBackground', function(){
+        	// add employee
+        	this.click('#MenuZone > div.JuiLeftTabStrip > div.JuiTabStripTabZoneOuter > div > div:nth-child(4)');
+        	this.waitUntilVisible('#MenuZone > div.JuiLeftTabStrip > div.JuiTabStripBodyZone > div:nth-child(4)', function(){
+        		console.log('system panel show');
+        		this.click('#MenuZone > div.JuiLeftTabStrip > div.JuiTabStripBodyZone > div:nth-child(4) > div > div > div:nth-child(1) > div > div > div > div:nth-child(1) > div.JuiTreeNodeBody.JuiTreeNodeBodyWithLine > div:nth-child(2) > div > div.JuiTreeTextCell.JuiTreeLeafIcon > div');
+      			this.waitForSelector('#TabZone > div > div.JuiTabStripBodyZone > iframe:nth-child(2)', function(){
+      				console.log('employee iframe show');
+      				this.page.switchToChildFrame(2);
+      				// to iframe 2
+      				this.click('#_JUI_1_ > div.JuiToolBarBody > div.JuiToolBarRight > div:nth-child(1) > div > div.JuiButtonText');
+      				this.page.switchToParentFrame();
+      				this.waitUntilVisible('body > div.JuiDialog', function(){
+      					console.log(this.getElementInfo('body > div.JuiDialog > div.JuiDialogBody').html);
+      					//console.log(this.getElementInfo(x('/html/body/div[8]/div[5]')).html);
+      					test.pass('employee iframe show success');
+      				})
+      				
+      			});
+        	})
+
+        })
+
     }).run(function() {
         test.done();
     });
